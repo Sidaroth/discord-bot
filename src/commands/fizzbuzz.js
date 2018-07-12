a744fcf94781529977800dfd29d22148c1d6353c
Christian Holt <sidaroth@protonmail.com>
2018-07-12 15:03:58 +0200

    Update fizzbuzz.js

--- src/commands/fizzbuzz.js
+++ src/commands/fizzbuzz.js
@@ -27,13 +27,13 @@ function fizzBuzzCurry(amount) {
 
 module.exports = {
     name: 'fizzbuzz',
-    description: 'Prints fizzbuzz from 1 to <amount> (cap. 250, default 100). (fizz, buzz, fuzz, bizz, biff, 3, 5, 7, 11, 13)',
+    description: 'Prints fizzbuzz from 1 to <amount> (cap. 250, default 25). (fizz, buzz, fuzz, bizz, biff, 3, 5, 7, 11, 13)',
     aliases: ['fizz', 'buzz'],
     usage: ['<amount>'],
     execute(message, args) {
-        let amount = parseInt(args[0]) || 100;
+        let amount = parseInt(args[0]) || 25;
         if (amount < 1) amount = 1;
-        // if (amount > 250) amount = 250;
+        if (amount > 250) amount = 250;
         message.channel.send(fizzBuzzCurry(amount));
     },
 };
