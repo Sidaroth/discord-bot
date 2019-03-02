0281b7d8de86111a865b16b691a26b315ebf0cde
Sidaroth <sidaroth@protonmail.com>
2019-03-02 17:07:32 +0100

    Ressurect the armory command after blizzard changed their API endpoints and authentication.

--- src/commands/armory.js
+++ src/commands/armory.js
@@ -1,8 +1,100 @@
 import secrets from '../../secrets.json';
+import tokens from '../db/tokens';
 import axios from 'axios';
 import Discord from 'discord.js';
 import getRaceString from '../utils/getWowRaceString';
 import getClassString from '../utils/getWowClassString';
+
+function getArmoryData(server, character) {
+    const uri = `https://eu.api.blizzard.com/wow/character/${server}/${character}?fields=titles,stats,guild,items&locale=en_GB&access_token=${
+        tokens.blizzard.access_token
+    }`;
+
+    return axios.get(uri);
+}
+
+function getRaiderIoData(server, character) {
+    const uri = `https://raider.io/api/v1/characters/profile?region=eu&realm=${server}&name=${character}&fields=mythic_plus_scores`;
+    return axios.get(uri);
+}
+
+function getAccessToken() {
+    const uri = 'https://us.battle.net/oauth/token?grant_type=client_credentials';
+    return axios.post(
+        uri,
+        {},
+        {
+            auth: {
+                username: secrets.blizzardClientId,
+                password: secrets.blizzardClientSecret,
+            },
+        },
+    );
+}
+
+function isTokenValid() {
+    const hasAccesToken = tokens.blizzard.access_token !== '';
+    const hasUpdateTime = tokens.blizzard.updateTime !== 0;
+    const hasNotExpired = tokens.blizzard.updateTime + tokens.blizzard.valid_for < Math.floor(Date.now() / 1000);
+
+    return hasAccesToken && hasUpdateTime && hasNotExpired;
+}
+
+function performQuery(message, server, character) {
+    axios.all([getArmoryData(server, character), getRaiderIoData(server, character)]).then(axios
+        .spread((armoryData, raiderIoData) => {
+            const {
+                name,
+                guild,
+                realm,
+                level,
+                titles,
+                faction,
+                stats,
+                gender,
+                battlegroup,
+                achievementPoints,
+                totalHonorableKills,
+                items,
+            } = armoryData.data;
+
+            const ilvl = items.averageItemLevelEquipped;
+            const maxIlvl = items.averageItemLevel;
+            const { health, power, powerType } = stats;
+            const factionString = faction === 1 ? 'Horde' : 'Alliance';
+            const factionColor = faction === 1 ? 'FF2211' : '0099FF';
+            const genderString = gender === 1 ? 'Female' : 'Male';
+            const guildName = guild.name;
+            const titleObject = titles.find(title => title.selected != null);
+            const fullName = titleObject ? titleObject.name.replace('%s', name) : name;
+            const powerString = powerType.charAt(0).toUpperCase() + powerType.substr(1);
+            const classString = getClassString(armoryData.data.class);
+            const raceString = getRaceString(armoryData.data.race);
+
+            const characterData = [];
+            characterData.push(`${name} is a level ${level}, ${genderString} ${raceString} ${classString}.`);
+            characterData.push(`Health: ${health}.`);
+            characterData.push(`${powerString}: ${power}.`);
+            characterData.push(`Faction: ${factionString} ${faction === 1 ? 'Scum!' : 'Swine!'}`);
+            characterData.push(`Achievement Points: ${achievementPoints}.`);
+            characterData.push(`"Honorable" kills: ${totalHonorableKills}.`);
+
+            const embed = new Discord.RichEmbed()
+                .setColor(factionColor)
+                .setTitle(`${fullName} (${ilvl}/${maxIlvl})`)
+                .setDescription(`Member of ${guildName} - ${realm} (${battlegroup})`)
+                .setURL(`https://worldofwarcraft.com/en-gb/character/${server}/${character}`)
+                .addField('Character Data', characterData.join('\n'));
+            message.channel.send(embed);
+        }))
+        .catch((err) => {
+            if (err.response.data.reason === 'Character not found.') {
+                message.channel.send("I'm afraid I coulnd't find any characters on that realm, with that name. If you believe this is an error, contact admin.");
+            } else {
+                console.error(err);
+            }
+        });
+}
 
 module.exports = {
     name: 'armory',
@@ -20,64 +112,23 @@ module.exports = {
         }
 
         const character = args.shift().toLowerCase();
-        const uri = `https://eu.api.battle.net/wow/character/${server}/${character}?fields=titles,stats,guild,items&locale=en_GB&apikey=${
-            secrets.blizzardAPIKey
-        }`;
 
-        axios
-            .get(uri)
-            .then((res) => {
-                const {
-                    name,
-                    guild,
-                    realm,
-                    level,
-                    titles,
-                    faction,
-                    stats,
-                    gender,
-                    battlegroup,
-                    achievementPoints,
-                    totalHonorableKills,
-                    items,
-                } = res.data;
+        if (isTokenValid()) {
+            performQuery(message, server, character);
+        } else {
+            // If the token is invalid, we need to request a new one first before we do any api queries.
+            getAccessToken()
+                .then((token) => {
+                    tokens.blizzard.valid_for = token.data.expires_in;
+                    tokens.blizzard.token_type = token.data.token_type;
+                    tokens.blizzard.access_token = token.data.access_token;
+                    tokens.blizzard.updateTime = Math.floor(Date.now() / 1000);
 
-                const ilvl = items.averageItemLevelEquipped;
-                const maxIlvl = items.averageItemLevel;
-                const { health, power, powerType } = stats;
-                const factionString = faction === 1 ? 'Horde' : 'Alliance';
-                const factionColor = faction === 1 ? 'FF2211' : '0099FF';
-                const genderString = gender === 1 ? 'Female' : 'Male';
-                const guildName = guild.name;
-                const titleObject = titles.find(title => title.selected != null);
-                const fullName = titleObject ? titleObject.name.replace('%s', name) : name;
-                const powerString = powerType.charAt(0).toUpperCase() + powerType.substr(1);
-                const classString = getClassString(res.data.class);
-                const raceString = getRaceString(res.data.race);
-
-                const characterData = [];
-                characterData.push(`${name} is a level ${level}, ${genderString} ${raceString} ${classString}.`);
-                characterData.push(`Health: ${health}.`);
-                characterData.push(`${powerString}: ${power}.`);
-                characterData.push(`Faction: ${factionString} ${faction === 1 ? 'Scum!' : 'Swine!'}`);
-                characterData.push(`Achievement Points: ${achievementPoints}.`);
-                characterData.push(`"Honorable" kills: ${totalHonorableKills}.`);
-
-                const embed = new Discord.RichEmbed()
-                    .setColor(factionColor)
-                    .setTitle(`${fullName} (${ilvl}/${maxIlvl})`)
-                    .setDescription(`Member of ${guildName} - ${realm} (${battlegroup})`)
-                    .setURL(`https://worldofwarcraft.com/en-gb/character/${server}/${character}`)
-                    .addField('Character Data', characterData.join('\n'));
-
-                return message.channel.send(embed);
-            })
-            .catch((err) => {
-                if (err.response.data.reason === 'Character not found.') {
-                    message.channel.send("I'm afraid I coulnd't find any characters on that realm, with that name. If you believe this is an error, contact admin.");
-                } else {
+                    performQuery(message, server, character);
+                })
+                .catch((err) => {
                     console.error(err);
-                }
-            });
+                });
+        }
     },
 };
---
+++ src/db/tokens.js
@@ -0,0 +1,9 @@
+// Storing access tokens while they are valid.
+export default {
+    blizzard: {
+        access_token: '',
+        token_type: '',
+        valid_for: 0,
+        updateTime: 0,
+    },
+};
