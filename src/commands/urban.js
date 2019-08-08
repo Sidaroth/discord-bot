0a236d6358171c18a7a896d732e9e8c9ae0680a1
Sidaroth <sidaroth@protonmail.com>
2019-08-08 16:12:14 +0200

    Ressurrect urban dictionary search after Api changes.

--- src/commands/urban.js
+++ src/commands/urban.js
@@ -1,6 +1,6 @@
 import Discord from 'discord.js';
-import snekfetch from 'snekfetch';
 import trim from '../utils/trim';
+import axios from 'axios';
 
 module.exports = {
     name: 'urban',
@@ -11,33 +11,32 @@ module.exports = {
     requiresArgs: true,
     execute(message, args) {
         const term = args.join(' ');
-        snekfetch
-            .get('https://api.urbandictionary.com/v0/define')
-            .query({ term }) // i.e https://api.urbandictionary.com/v0/define?term=hello%20world
-            .then((res) => {
-                if (res.body.result_type !== 'no_results') {
-                    const { tags, list } = res.body;
-                    const top = list.sort((l, r) => {
-                        if (l.thumbs_up < r.thumbs_up) return 1;
-                        if (l.thumbs_up > r.thumbs_up) return -1;
-                        return 0;
-                    })[0];
+        const uri = `https://api.urbandictionary.com/v0/define?term=${term}`;
 
-                    const example = trim(top.example, 1024) || 'No example found.';
-                    const embed = new Discord.RichEmbed()
-                        .setColor('EFFF00')
-                        .setTitle(top.word)
-                        .setURL(top.permalink)
-                        .addField('Definition', trim(top.definition, 1024))
-                        .addBlankField(false)
-                        .addField('Example', example)
-                        .addBlankField(false)
-                        .addField('Rating', `${top.thumbs_up} thumbs up.\n${top.thumbs_down} thumbs down.`)
-                        .setFooter(`Tags: ${tags.join(', ')}`);
-                    message.channel.send(embed);
-                } else {
-                    message.channel.send(`Urban dictionary doesn't seem to have a definition for *${term}*`);
-                }
-            });
+        axios.get(uri).then((res) => {
+            if (res.data.list.length === 0) {
+                return message.channel.send(`Urban dictionary doesn't seem to have a definition for *${term}*`);
+            }
+
+            const { list } = res.data;
+            const top = list.sort((l, r) => {
+                if (l.thumbs_up < r.thumbs_up) return 1;
+                if (l.thumbs_up > r.thumbs_up) return -1;
+                return 0;
+            })[0];
+
+            const example = trim(top.example, 1024) || 'No example found.';
+            const embed = new Discord.RichEmbed()
+                .setColor('EFFF00')
+                .setTitle(top.word)
+                .setURL(top.permalink)
+                .addField('Definition', trim(top.definition, 1024))
+                .addBlankField(false)
+                .addField('Example', example)
+                .addBlankField(false)
+                .addField('Rating', `${top.thumbs_up} thumbs up.\n${top.thumbs_down} thumbs down.`);
+
+            return message.channel.send(embed);
+        });
     },
 };
