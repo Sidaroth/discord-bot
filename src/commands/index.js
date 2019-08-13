160ef53cb90c09d2ec68c248a47652baa6c30c03
Sidaroth <sidaroth@protonmail.com>
2019-08-13 12:13:19 +0200

    EGS meme command.

--- src/commands/index.js
+++ src/commands/index.js
@@ -15,6 +15,7 @@ import prune from './prune';
 import racist from './racist';
 import rank from './rank';
 import stats from './stats';
+import sweeney from './sweeney';
 import triggered from './triggered';
 import urban from './urban';
 import usage from './usage';
@@ -40,6 +41,7 @@ const commands = [
     rank,
     role,
     stats,
+    sweeney,
     triggered,
     urban,
     usage,
---
+++ src/commands/sweeney.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'sweeney',
+    description: 'EGS memes?',
+    aliases: ['egs'],
+    cooldown: 5,
+    execute: async (message, args) => {
+        message.channel.send('https://cdn.discordapp.com/attachments/138517832920072193/610776439289282598/1562094187945.png');
+    },
+};
