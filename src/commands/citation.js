0fee7421cf57098e2a983a74db5b6e3d1da7881f
Sidaroth <sidaroth@protonmail.com>
2019-08-25 16:28:15 +0200

    Updated citation needed command.

--- src/commands/citation.js
+++ src/commands/citation.js
@@ -1,9 +1,12 @@
+import { Attachment } from 'discord.js';
+
 module.exports = {
     name: 'citation',
     description: 'Kindly lets the user know that a citation is needed!',
     cooldown: 5,
     aliases: ['cn'],
     execute(message, args) {
-        return message.channel.send('https://xkcd.com/285/');
+        const attachment = new Attachment('https://i.imgur.com/nb0HLsT.png');
+        return message.channel.send(attachment);
     },
 };
